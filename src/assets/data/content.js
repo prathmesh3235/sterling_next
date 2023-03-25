24d7352e8c95192037e231b7016b44fe6a001bab
prathmesh3235 <prathmesh32352@gmail.com>
2023-03-25 01:51:12 +0100

    updated jason and images

--- src/assets/data/content.js
+++ src/assets/data/content.js
@@ -5,8 +5,8 @@ const data = {
 			description: "Category Desc",
 			products: [
 				{
-					imageURL: "/images/Earings/PSCZ01.jpg",
-					id: "PSCZ01",
+					imageURL: "/images/Earings/PSECZ01.jpg",
+					id: "PSECZ01",
 					price: 1234,
 					features: [
 						"Daimeter: 8.9mm",
@@ -20,8 +20,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ02.jpg",
-					id: "PSCZ02",
+					imageURL: "/images/Earings/PSECZ02.jpg",
+					id: "PSECZ02",
 					price: 1234,
 					features: [
 						"Length: 16.9mm",
@@ -35,8 +35,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ03.jpg",
-					id: "PSCZ03",
+					imageURL: "/images/Earings/PSECZ03.jpg",
+					id: "PSECZ03",
 					price: 1234,
 					features: [
 						"Length: 18.5mm",
@@ -51,8 +51,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ04.jpg",
-					id: "PSCZ04",
+					imageURL: "/images/Earings/PSECZ04.jpg",
+					id: "PSECZ04",
 					price: 1234,
 					features: [
 						"Length: 17.7mm",
@@ -67,8 +67,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ05.jpg",
-					id: "PSCZ05",
+					imageURL: "/images/Earings/PSECZ05.jpg",
+					id: "PSECZ05",
 					price: 1234,
 					features: [
 						"Inner Diameter:9mm",
@@ -84,8 +84,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ06.jpg",
-					id: "PSCZ06",
+					imageURL: "/images/Earings/PSECZ06.jpg",
+					id: "PSECZ06",
 					price: 1234,
 					features: [
 						"Length:23.5mm",
@@ -99,8 +99,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ07.jpg",
-					id: "PSCZ07",
+					imageURL: "/images/Earings/PSECZ07.jpg",
+					id: "PSECZ07",
 					price: 1234,
 					features: [
 						"Length: 6mm",
@@ -115,8 +115,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ08.jpg",
-					id: "PSCZ08",
+					imageURL: "/images/Earings/PSECZ08.jpg",
+					id: "PSECZ08",
 					price: 1234,
 					features: [
 						"Inner Diamter: 8.5mm",
@@ -132,8 +132,8 @@ const data = {
 					outofstock: true,
 				},
 				{
-					imageURL: "/images/Earings/PSCZ09.jpg",
-					id: "PSCZ09",
+					imageURL: "/images/Earings/PSECZ09.jpg",
+					id: "PSECZ09",
 					price: 1234,
 					features: [
 						"Length: 20.70mm",
@@ -150,15 +150,225 @@ const data = {
 				},
 			],
 		},
-	],
-
-	Bracelets: [
 		{
-			name: "Bracelets 1",
-			description: "asdeqwasewqs",
+			name: "Gemstone",
+			description: "Category Desc",
 			products: [
 				{
-					imageURL: "/images/braceletsSlider.jpg",
+					imageURL: "/images/Earings/PSEGS01.jpg",
+					id: "PSEGS01",
+					price: 1234,
+					features: [
+						"Length: 21.5mm",
+						"Width: 9.5mm",
+						"Weight:3.1 grams",
+						"Material: Nickel Free Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Silver Earrings with Moon Stone",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+			]
+	    },
+		{
+			name: "Hearts",
+			description: "Category Desc",
+			products: [
+				{
+					imageURL: "/images/Earings/PSEHE01.jpg",
+					id: "PSEHE01",
+					price: 1234,
+					features: [
+						"Length: 8.6 mm",
+						"Width: 9.6 mm",
+						"Weight: 1.4 grams",
+						"Material: Nickel Free Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Plain Silver Heart Earrings",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+				{
+					imageURL: "/images/Earings/PSEHE02.jpg",
+					id: "PSEHE02",
+					price: 1234,
+					features: [
+						"Length: 8.9 mm",
+						"Width: 8.9 mm",
+						"Weight: 1.5 grams",
+						"Material: Nickel Free Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Silver Heart Earring with CZ",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+			]
+	    },
+
+		{
+			name: "Hoops",
+			description: "Category Desc",
+			products: [
+				{
+					imageURL: "/images/Earings/PSEHO01.jpg",
+					id: "PSEHO01",
+					price: 1234,
+					features: [
+						"Length: 11 mm",
+						"Width: 6.5 mm",
+						"Weight: 3.3 grams",
+						"Material: Nickel Free Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Silver Plain Hoops with Yellow Gold Plating",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+				{
+					imageURL: "/images/Earings/PSEHO02.jpg",
+					id: "PSEHO02",
+					price: 1234,
+					features: [
+						"Length: 17..3 mm",
+						"Width: 8.3 mm",
+						"Weight: 5.4 grams",
+						"Material: Sterling Silver",
+						"Plating: Yellow Gold Plated",
+					],
+					description: "Plain Silver Hoop Earrings with Yellow Gold Plated",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+			]
+	    },
+		{
+			name: "Plain Silver",
+			description: "Category Desc",
+			products: [
+				{
+					imageURL: "/images/Earings/PSEPS01.jpg",
+					id: "PSEPS01",
+					price: 1234,
+					features: [
+						"Length: 13.5 mm",
+						"Width: 1.7 mm",
+						"Weight: 4 grams",
+						"Material: Nickel Free Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Plain Silver Leaf-Hoop Earrings",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+				{
+					imageURL: "/images/Earings/PSEPS2.jpg",
+					id: "PSEPS02",
+					price: 1234,
+					features: [
+						"Length: 10 mm",
+						"Width: 4.15 mm",
+						"Weight: 1.25 grams",
+						"Material: Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Plain Silver Earrings with Yellow Gold Plating",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+				{
+					imageURL: "/images/Earings/PSEPS03.jpg",
+					id: "PSEPS03",
+					price: 1234,
+					features: [
+						"Length: 12.7 mm",
+						"Width: 7.6 mm",
+						"Weight: 0.93 grams",
+						"Material: Nickel Free Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Plain Silver Earrings with Yellow Gold Plated",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+				{
+					imageURL: "/images/Earings/PSEPS04.jpg",
+					id: "PSEPS04",
+					price: 1234,
+					features: [
+						"Length: 13.5 mm",
+						"Width: 16 mm",
+						"Weight: 4.25 grams",
+						"Material: Nickel Free Sterling Silver",
+						
+					],
+					description: "Plain Silver Hoops Earrings with Yellow Gold Plating",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+
+
+			]
+	    },
+
+		{
+			name: "Studs",
+			description: "Category Desc",
+			products: [
+				{
+					imageURL: "/images/Earings/PSEST01.jpg",
+					id: "PSEST01",
+					price: 1234,
+					features: [
+						//"Length: 11 mm",
+						//"Width: 6.5 mm",
+						//"Weight: 3.3 grams",
+						//"Material: Nickel Free Sterling Silver",
+						//"Plating: Rhodium",
+					],
+					description: "Silver Stud Earrings with Clear Crystals",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+				{
+					imageURL: "/images/Earings/PSEST02.jpg",
+					id: "PSEST02",
+					price: 1234,
+					features: [
+						//"Length: 17..3 mm",
+						//"Width: 8.3 mm",
+						"Weight: 0.25 grams",
+						"Material: Sterling Silver",
+						"Plating: Rhodium",
+					],
+					description: "Silver Earring with 3mm Round Stud CZ",
+					bestselling: true,
+					instock: true,
+					outofstock: true,
+				},
+			]
+	    }
+	
+	],
+
+	Bracelets: [		{
+			name: "Cubic Zirconia (CZ)",
+			description: "Description",
+			products: [
+				{
+					imageURL: "/images/Bracelets/PSBCZ01.jpg",
 					name: "qwert0",
 					id: "EZ1144",
 					price: 125412,
@@ -198,7 +408,137 @@ const data = {
 	],
 	Necklaces: [
 		{
-			name: "Necklaces 1",
+			name: "Cubic Zircona",
+			description: "Description",
+			products: [
+				{
+					imageURL: "/images/Neclaces/PSNCZ01.jpg",
+					id: "PSNCZ01",
+					price: 125412,
+					features: ["Length: 16''+2'' Ext.", 
+					"Pendant Diameter: 8.2mm", 
+					"Weight: 2.5 grams", "Material: Nickel Free Sterling Silver", "Plating: Rhodium "],
+					description: "Silver Heart Necklace with White CZ Stone",
+				},
+				{
+					imageURL: "/images/Neclaces/PSNCZ02.jpg",
+					id: "PSNCZ02",
+					price: 125412,
+					features: ["Length: 16''+2'' Ext.", 
+					"Pendant Diameter: 14.5 mm with bail", 
+					"Weight: 2.5 grams", "Material: Nickel Free Sterling Silver", "Plating: Rhodium "],
+					description: "Silver Turtle Necklace with White CZ Stones",
+				},
+				{
+					imageURL: "/images/Neclaces/PSNCZ03.jpg",
+					id: "PSNCZ03",
+					price: 125412,
+					features: ["Length: 16''+2'' Ext.", 
+					"Pendant Length: 11.4 mm with bail", 
+					"Pendant Width: 9.7mm",
+					"Weight:1.4 grams", "Material: Nickel Free Sterling Silver", "Plating: Rhodium "],
+					description: "Silver Evil Eye Necklace with White CZ Stones and Yellow Gold Plating",
+				},
+			],
+		},
+		{
+			name: "Hearts",
+			description: "Description",
+			products: [
+				{
+					imageURL: "/images/Neclaces/PSNH01.jpg",
+					id: "PSNH01",
+					price: 125412,
+					features: ["Length: 16''+1'' Ext.", 
+					"Pendant Length: 7.8mm", 
+					"Pendant Width: 8mm",
+					"Material: Nickel Free Sterling Silver",
+					"Plating: Rhodium",
+				
+				],
+					description: "Plating: Rhodium",
+				},
+				{
+					imageURL: "/images/Neclaces/PSNH02.jpg",
+					id: "PSNH02",
+					price: 125412,
+					features: ["Length: 16''+2'' Ext.", 
+					"Pendant Length: 17 mm with Bail", 
+					"Pendant Width: 14.7 mm",
+					"Weight: 2.4 grams",
+					"Material: Nickel Free Sterling Silver",
+					"Plating: Rhodium",
+				
+				],
+					description: "Weight: 2.4 grams",
+				},
+				{
+					imageURL: "/images/Neclaces/PSNH03.jpg",
+					id: "PSNH03",
+					price: 125412,
+					features: ["Chain Length: 16'' + 1.5'' extension", 
+					"Heart Length: 10mm", 
+					"Heart Width: 11.8mm",
+					"Weight of Necklace: 2.4 grams",
+					"Material: Nickel Free Sterling Silver",
+					"Plating: Rhodium",
+				
+				],
+					description: "Silver Heart Necklace with Cubic Zirconia. Center Stone Dances",
+				},
+			],
+		},
+		{
+			name: "Plain Silver",
+			description: "description",
+			products: [
+				{
+					imageURL: "/images/Earrings/PSEPS01.jpg",
+					id: "PSEPS01",
+					price: 125412,
+					features: ["Length: 16'' +2'' Ext.",
+					 "Length Pendant: 7.8mm",
+					 "Width Pendant: 7mm",
+					"Weight: 2.7 grams",
+					"Material: Nickel Free Sterling Silver",
+					"Plating: Rhodium"
+
+					],
+					description: "Plain Silver Double Layered Heart Necklace",
+				},
+				{
+					imageURL: "/images/Earrings/PSEPS02.jpg",
+					id: "PSEPS02",
+					price: 125412,
+					features: ["Length: 16'' +1'' Ext.",
+					 "Length Pendant: 7.2 mm",
+					 "Width Pendant: 6.9 mm",
+					"Weight: 2.30 grams",
+					"Material: Nickel Free Sterling Silver",
+					"Plating: Rhodium"
+
+					],
+					description: "Plain Silver Necklace",
+				},
+				{
+					imageURL: "/images/Earrings/PSEPS03.jpg",
+					id: "PSEPS03",
+					price: 125412,
+					features: ["Length: 16'' +2'' Ext.",
+					 "Length Pendant: 24 mm",
+					 "Width Pendant: 14.30 mm",
+					"Weight: 4 grams",
+					"Material: Nickel Free Sterling Silver",
+					"Plating: Rhodium"
+
+					],
+					description: "Plain Silver Cross Necklace",
+				},
+				
+			],
+		},
+		{
+			name: "Necklaces 4",
 			description: "asdeqwasewqs",
 			products: [
 				{
@@ -212,21 +552,7 @@ const data = {
 			],
 		},
 		{
-			name: "Necklaces 2",
-			description: "asdeqwasewqs",
-			products: [
-				{
-					imageURL: "/images/braceletsSlider.jpg",
-					name: "qwert0",
-					id: "EZ1144",
-					price: 125412,
-					features: ["features 1", "features 2", "features 3"],
-					description: "qwertyu",
-				},
-			],
-		},
-		{
-			name: "Necklaces 3",
+			name: "Necklaces 5",
 			description: "asdeqwasewqs",
 			products: [
 				{
