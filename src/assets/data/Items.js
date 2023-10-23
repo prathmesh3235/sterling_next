978e8caf13917fe98735bf4a692aba28892eadbb
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2023-10-23 19:03:22 +0200

    Updated slider image

--- src/assets/data/Items.js
+++ src/assets/data/Items.js
@@ -1,30 +1,36 @@
 const Items = {
-	items: {
-		bootstrap: [
-			{
-				id: 1,
-				title: "Rings",
-				body: "Random Fancy Ring Name",
-				imageUrl: "/images/sliderbg1.jpg",
-				docs: "https://getbootstrap.com/docs/4.0/components/carousel/",
-			},
-			{
-				id: 2,
-				title: "Necklaces",
-				body: "Random Necklace Name",
-				imageUrl: "/images/sliderbg2.jpg",
-				docs: "https://getbootstrap.com/docs/4.0/components/carousel/",
-			},
-			{
-				id: 3,
-				title: "Diamond",
-				body: "Random Diamond Name",
-				imageUrl: "/images/sliderbg6.jpg",
-				docs: "https://getbootstrap.com/docs/4.0/components/carousel/",
-			},
-			
-		],
-	},
+  items: {
+    bootstrap: [
+      {
+        id: 1,
+        title: "Varshini",
+        body: "Slide",
+        imageUrl: "/images/bgimg5.jpeg",
+        docs: "https://getbootstrap.com/docs/4.0/components/carousel/",
+      },
+      {
+        id: 2,
+        title: "Rings",
+        body: "Random Fancy Ring Name",
+        imageUrl: "/images/sliderbg1.jpg",
+        docs: "https://getbootstrap.com/docs/4.0/components/carousel/",
+      },
+      {
+        id: 3,
+        title: "Necklaces",
+        body: "Random Necklace Name",
+        imageUrl: "/images/sliderbg2.jpg",
+        docs: "https://getbootstrap.com/docs/4.0/components/carousel/",
+      },
+      {
+        id: 4,
+        title: "Diamond",
+        body: "Random Diamond Name",
+        imageUrl: "/images/sliderbg6.jpg",
+        docs: "https://getbootstrap.com/docs/4.0/components/carousel/",
+      },
+    ],
+  },
 };
 
 export default Items;
